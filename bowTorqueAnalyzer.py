9f4a141f095c3b83737530aac2e6013ef8390142
victorc <victorc@optimaldesignco.com>
2022-09-29 14:09:30 -0500

    Created VS code python project for the bow torque analyzing.

---
+++ bowTorqueAnalyzer.py
@@ -0,0 +1,27 @@
+# === IMPORTS ==================================================================
+import enum
+import glob
+import math
+import operator
+import os
+import shutil
+import string
+import typing
+import xlsxwriter
+
+# === GLOBAL CONSTANTS =========================================================
+
+
+# === FUNCTIONS ================================================================
+
+def process():
+    print('running bowTorqueAnalyzer...')
+
+
+# === MAIN =====================================================================
+
+if __name__ == "__main__":
+    process()
+else:
+    print("ERROR: bowTorqueAnalyzer needs to be the calling python module!")
+    
